01cf0dbd366524ec95dcbdebcb9c8c950270fd92
sin <929289406@qq.com>
2019-03-12 20:51:52 +0800

    fix

--- data/items.py
+++ data/items.py
@@ -5,3 +5,6 @@ class DataItem(scrapy.Item):
     id = scrapy.Field()
     name = scrapy.Field()
     update_time = scrapy.Field()
+    commits = scrapy.Field()
+    branches = scrapy.Field()
+    releases = scrapy.Field()
--- data/spiders/github.py
+++ data/spiders/github.py
@@ -5,15 +5,25 @@ class GithubSpider(scrapy.Spider):
     name = 'github'
 
     @property
-    def start_usrls(self):
-        return (['https://github.com/shiyanlou?tab=repositories',
+    def start_urls(self):
+        return ['https://github.com/shiyanlou?tab=repositories',
         'https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK0MjAxNy0wNi0wNlQyMjoyMToxNlrOBZKV2w%3D%3D&tab=repositories',
         'https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK0MjAxNS0wMS0yNlQwODoxNzo1M1rOAcd_9A%3D%3D&tab=repositories',
-        'https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK0MjAxNS0wMS0yNlQwODoxNzo1M1rOAcd_9A%3D%3D&tab=repositories'])
+        'https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK0MjAxNS0wMS0yNlQwODoxNzo1M1rOAcd_9A%3D%3D&tab=repositories']
+
     def parse(self, response):
         for rep in response.css('li.public'):
-            item = DataItem({
-            'name':rep.xpath('.//a[@itemprop="name codeRepository"]/text()').re_first(r'\n\s*(.*)'),
-            'update_time':rep.xpath('.//relative-time/@datetime').extract_first()
-            }) 
-            yield item
+            item = DataItem()
+            item['name'] = rep.xpath('.//a[@itemprop="name codeRepository"]/text()').re_first(r'\n\s*(.*)')
+            item['update_time'] = rep.xpath('.//relative-time/@datetime').extract_first()
+            data_url = response.urljoin(rep.xpath('.//h3/a/@href').extract_first())
+            request = scrapy.Request(data_url, callback=self.parse_data)
+            request.meta['item'] = item
+            yield request
+
+    def parse_data(self, response):
+        item = response.meta['item']
+        item['commits'] = response.xpath('.//li[1]//a/span/text()').extract()
+        item['branches'] = response.xpath('.//li[2]/a/span/text()').extract()
+        item['releases'] = response.xpath('.//li[3]/a/span/text()').extract()
+        yield item
